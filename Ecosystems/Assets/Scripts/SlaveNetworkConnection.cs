5769e60fd84e2039b213f07c6927b371f9e799ab
Samy Barras <samy.barras@gmail.com>
2018-12-10 12:12:57 +0100

    Client Network test
    script for client on unity network

---
+++ Ecosystems/Assets/Scripts/SlaveNetworkConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SlaveNetworkConnection : MonoBehaviour {
+
+    public bool isAtStartup = true;
+    NetworkClient myClient;
+    void Update()
+    {
+        if (isAtStartup)
+        {
+            SetupClient();
+        }
+    }
+
+    // Create a client and connect to the server port
+    public void SetupClient()
+    {
+        myClient = new NetworkClient();
+        myClient.RegisterHandler(MsgType.Connect, OnConnected);
+        myClient.Connect("192.168.0.26", 4444);
+        isAtStartup = false;
+    }
+
+    // client function
+    public void OnConnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Connected to server");
+    }
+
+}
